164670945d4a8803d735aa6927363359e3fc03ae
chordpli <chordplaylist@gmail.com>
2022-12-19 11:21:51 +0900

    feat: idGenerator, 날짜 포맷 변경

--- src/main/java/com/ll/exam/app_2022_12_19__1/ChatMessage.java
+++ src/main/java/com/ll/exam/app_2022_12_19__1/ChatMessage.java
@@ -1,5 +1,6 @@
 package com.ll.exam.app_2022_12_19__1;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -8,12 +9,21 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 @Getter
 public class ChatMessage {
-    private long id;
+    private Long id;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
     private LocalDateTime createDate;
     private String authorName;
     private String content;
 
     public ChatMessage(String authorName, String content){
-        this(1, LocalDateTime.now(), authorName, content);
+        this(ChatMessageIdGenerator.genNextId(), LocalDateTime.now(), authorName, content);
+    }
+}
+
+class ChatMessageIdGenerator{
+    private static long id = 0;
+
+    public static long genNextId(){
+        return ++id;
     }
 }
