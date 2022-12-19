72e839002afe98e7782d0deefe7cce01aa4552c7
chordpli <chordplaylist@gmail.com>
2022-12-19 11:21:29 +0900

    feat: 메세지 범위 정하기. 반복문 -> 스트림

--- src/main/java/com/ll/exam/app_2022_12_19__1/controller/ChatController.java
+++ src/main/java/com/ll/exam/app_2022_12_19__1/controller/ChatController.java
@@ -2,19 +2,102 @@ package com.ll.exam.app_2022_12_19__1.controller;
 
 import com.ll.exam.app_2022_12_19__1.ChatMessage;
 import com.ll.exam.app_2022_12_19__1.RsData;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
 
 @Controller
 @RequestMapping("/chat")
+@Slf4j
 public class ChatController {
+
+    private List<ChatMessage> chatMessages = new ArrayList<>();
+
+    @AllArgsConstructor
+    @Getter
+    public static class writeMessageResponse {
+        private final long id;
+    }
+
+    @AllArgsConstructor
+    @Getter
+    public static class writeMessageRequest {
+        private final String authorName;
+        private final String content;
+    }
+
+
     @PostMapping("/writeMessage")
     @ResponseBody
-    public RsData<ChatMessage> writeMessage(){
-        ChatMessage message = new ChatMessage("홍길동", "안녕하세요");
+    public RsData<writeMessageResponse> writeMessage(@RequestBody writeMessageRequest dto) {
+        // 채팅 메세지 생성
+        ChatMessage message = new ChatMessage(dto.getAuthorName(), dto.getContent());
+        // 채팅 메세지가 들어오면 저장.
+        chatMessages.add(message);
 
-        return new RsData( "S-1", "메세지가 작성되었습니다.", message);
+        return new RsData("S-1", "메세지가 작성되었습니다.", new writeMessageResponse(message.getId()));
+    }
+
+    @AllArgsConstructor
+    @Getter
+    public static class MessagesResponse {
+        private final List<ChatMessage> messages;
+        private final long count;
+    }
+
+    @AllArgsConstructor
+    @Getter
+    public static class MessagesRequest {
+        private final Long fromId;
+    }
+
+    @GetMapping("/messages")
+    @ResponseBody
+    public RsData<List<ChatMessage>> readMessage(MessagesRequest req) {
+        List<ChatMessage> messages = chatMessages;
+
+        // 번호가 입력 되었을 경우.
+        if (req.fromId != null) {
+            // 해당 번호의 채팅 메세지가 전체 리스트에서의 배열 인덱스 번호를 구함.
+            int index = IntStream.range(0, messages.size())
+                    .filter(i -> chatMessages.get(i).getId() == (req.fromId))
+                    .findFirst()
+                    .orElse(-1);
+
+            if (index != -1) {
+                // 만약에 index가 있으면, 0번부터 index 번까지 제거한 리스트를 만든다.
+                messages = messages.subList(index + 1, messages.size());
+            }
+        }
+
+        log.debug("req : {}", req);
+        return new RsData(
+                "S-1",
+                "메세지가 작성되었습니다.",
+                new MessagesResponse(messages, messages.size())
+        );
     }
 }
+
+    /*@GetMapping("/messages")
+    @ResponseBody
+    public RsData<List<ChatMessage>> readMessage(@RequestParam(required = false) Long fromId){
+
+        if (fromId == null) {
+            return new RsData( "S-1", "메세지가 작성되었습니다.", new MessagesResponse(chatMessages, chatMessages.size()));
+        }else{
+            List<ChatMessage> fromIdChatMessage = new ArrayList<>();
+            for (int i = 0; i < chatMessages.size() ; i++) {
+                if (chatMessages.get(i).getId() > fromId) {
+                    fromIdChatMessage.add(chatMessages.get(i));
+                }
+            }
+            return new RsData( "S-1", "메세지가 작성되었습니다.", new MessagesResponse(fromIdChatMessage, fromIdChatMessage.size()));
+        }
+    }*/
