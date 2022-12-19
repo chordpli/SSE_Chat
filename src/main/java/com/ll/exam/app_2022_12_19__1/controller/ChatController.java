730b18bac49de3b30598cdf5d4df86cf472506d7
chordpli <chordplaylist@gmail.com>
2022-12-19 10:27:13 +0900

    feat: post api "writeMessage"
    1. message 추가
    2. RsData형식으로 return

--- src/main/java/com/ll/exam/app_2022_12_19__1/controller/ChatController.java
+++ src/main/java/com/ll/exam/app_2022_12_19__1/controller/ChatController.java
@@ -1,5 +1,7 @@
 package com.ll.exam.app_2022_12_19__1.controller;
 
+import com.ll.exam.app_2022_12_19__1.ChatMessage;
+import com.ll.exam.app_2022_12_19__1.RsData;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,7 +12,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class ChatController {
     @PostMapping("/writeMessage")
     @ResponseBody
-    public String writeMessage(){
-        return "메세지가 작성되었습니다.";
+    public RsData<ChatMessage> writeMessage(){
+        ChatMessage message = new ChatMessage("홍길동", "안녕하세요");
+
+        return new RsData( "S-1", "메세지가 작성되었습니다.", message);
     }
 }
